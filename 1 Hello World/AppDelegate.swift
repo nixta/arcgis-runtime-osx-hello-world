982990dd6e8606ac2236a5c792e916bd9efc55ad
Nicholas Furness <nfurness@nixta.com>
2015-02-05 15:31:55 -0500

    Initial Commit

---
+++ 1 Hello World/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  1 Hello World
+//
+//  Created by Nicholas Furness on 2/5/15.
+//  Copyright (c) 2015 Nicholas Furness. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 1 Hello World/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  1 Hello World
+//
+//  Created by Nicholas Furness on 2/5/15.
+//  Copyright (c) 2015 Nicholas Furness. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
